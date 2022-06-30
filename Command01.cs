c339d90c602dedee02e08616042ab3dd653046c5
Mark Landis <landis.mark@frontier.com>
2022-06-29 20:25:05 -0400

    Session01 finished

--- Command01.cs
+++ Command01.cs
@@ -37,58 +37,28 @@ namespace Sesson1
             double offset = 0.05;
             double offsetcalc = offset * doc.ActiveView.Scale;
 
-            XYZ curpoint = new XYZ (0, 0, 0);
+            XYZ curPoint = new XYZ (0, 0, 0);
             XYZ offsetPoint = new XYZ(0,offsetcalc, 0);
 
-
-            XYZ pnt1 = new XYZ(0,0,0); 
-            XYZ pnt2 = new XYZ(1,1,1);
-
-            double num3 = num1 + num2;
-            double num4 = 5 + 5;
-            double num5 = num4 % 2;
 
             List<string> lst1 = new List<string>();
             //strings.Add("item 1");
             //strings.Add("item 2");
-
-            List<XYZ> points = new List<XYZ>();
-            points.Add(pnt1);
-            points.Add(pnt2);
-
-            int range = 100;
-            for(int i = 1;i<=100;i++)  //Defines for loop
-            {
-                num1 = num1 + 1;
-            }
-
-            string newString = "";
-            foreach(string s in strings)
-            {
-                if(s == "item1")
-                {
-                    newString = "got to 1";
-                }
-                else if (s == "item2")
-                {
-                    newString = "got to 2";
-                }
-                else
-                {
-                    newString = "got somewhere else";
-                }
-
-                newString = newString + s;
-            }
-
-            double num6 = Method01(100, 100);
 
             FilteredElementCollector collector = new FilteredElementCollector(doc);
             collector.OfClass(typeof(TextNoteType));
 
             Transaction t = new Transaction(doc, "Create Text Note");
             t.Start();
-            TextNote myText = new TextNote.Create(doc, doc.ActiveView.Id, pnt1, "This is my text note", collector.FirstElementId);
+
+            int range = 100;
+            for (int i = 1; i <= 100; i++)  //Defines for loop
+            {
+                TextNote curNote = TextNote.Create(doc, doc.ActiveView.Id, curPoint, "This is line " + i.ToString(), collector.FirstElementId());
+                curPoint = curPoint.Subtract(offsetPoint);
+            }
+
+
             t.Commit();
             t.Dispose();
 
